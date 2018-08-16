0bd71e15bc60877f4ac2db8bff9335bd46edb8a1
cdswyda <cdswyda@gmail.com>
2018-08-16 17:10:45 +0800

    add config

---
+++ config.demo.js
@@ -0,0 +1,10 @@
+module.exports = {
+    port: process.env.APP_PORT || '2222',
+    db: {
+        servername: '127.0.0.1',
+        database: "pref",
+        user: "",
+        pass: "",
+        authSource: ""
+    }
+}
