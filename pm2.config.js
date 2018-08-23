c475aeb12f3036f4ddfa4b92aaf80fbb45e92dad
cdswyda <cdswyda@gmail.com>
2018-08-23 16:18:31 +0800

    update pm2 config

--- pm2.config.js
+++ pm2.config.js
@@ -1,10 +1,15 @@
 const params = process.argv.slice(2)
+console.log(params);
 // [ 'start', 'pm2.config.js', '--node-args', 'dev' ]
 module.exports = {
     apps: [{
         "name": "Perf-Appraisal-Server",
         "script": "app.js",
-        "watch": params[3] === 'dev' ? true : false,
+        "watch": params[3] === 'dev' ? ['app.js', './app/**'] : false,
+        // "watch": ["app"],
+        "cwd": "./",
+        "node_args":"--harmony",
+        
         "error_file": "./logs/server-err.log",
         "out_file": "./logs/server.log",
         "env": {
