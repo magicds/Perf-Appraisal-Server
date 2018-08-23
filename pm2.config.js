e3b6e34e2dd190722b39fd66919004302aedb6b8
cdswyda <cdswyda@gmail.com>
2018-08-23 10:24:45 +0800

    update pm2 config

--- pm2.config.js
+++ pm2.config.js
@@ -1,10 +1,10 @@
-const params = process.argv.splice(2)
-
+const params = process.argv.slice(2)
+// [ 'start', 'pm2.config.js', '--node-args', 'dev' ]
 module.exports = {
     apps: [{
         "name": "Perf-Appraisal-Server",
         "script": "app.js",
-        "watch": params[0] === 'dev' ? true : false,
+        "watch": params[3] === 'dev' ? true : false,
         "error_file": "./logs/server-err.log",
         "out_file": "./logs/server.log",
         "env": {
